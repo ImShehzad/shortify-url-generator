fd471a1e50c61cf72c6889cf33a782acdcc68dd9
Muhammad Shehzad <engineer.muhammadshehzad@gmail.com>
2026-08-12 18:02:56 +0500

    Building web with production ready

--- Backend/db.js
+++ Backend/db.js
@@ -1,13 +1,11 @@
 const { Pool } = require("pg");
-require('dotenv').config();
-
+require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.PS_USER,            // your PostgreSQL username
-  host: process.env.PS_HOST,           // PostgreSQL server address
-  database: process.env.PS_DB,     // your DB name
-  password: process.env.PS_PASS,    // your PostgreSQL password
-  port: process.env.PS_PORT,                  // default PostgreSQL port
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
