37b67bd3e41eefcb66423aa3b5f57d00f9ef05b3
Muhammad Shehzad <engineer.muhammadshehzad@gmail.com>
2026-08-12 18:11:51 +0500

    Building web with production ready

--- Backend/backend.js
+++ Backend/backend.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: "https://tinyport.netlify.app",
+    origin: "https://shortify-url-generator-3.onrender.com"
   })
 );
 app.use(express.json());
